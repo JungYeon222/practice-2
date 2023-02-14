48c8a899217bfb352758135b0cda3a6499e44b7b
JungYeon222 <dlwjddus16@naver.com>
2023-02-14 22:25:11 +0900

    initial

---
+++ src/main/java/com/example/practice1/Controller.java
@@ -0,0 +1,14 @@
+package com.example.practice1;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "To-do Application !";
+    }
+}
